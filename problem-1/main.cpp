cfb377ed6c0f208b6fa9d83b18ddb43fdd16e98d
alaaahmad <44245806+alaaahmad@users.noreply.github.com>
2018-12-15 20:46:42 +0200

    Update main.cpp

--- problem-1/main.cpp
+++ problem-1/main.cpp
@@ -2,5 +2,19 @@
 
 int **transpose(int **y, int n) {
     // Write your code here
+    int a[10][10], trans[10][10], r, c, i, j;
+	for (i = 0; i < r; ++i)
+	for (j = 0; j < c; ++j)
+	
+	for (i = 0; i < r; ++i)
+	for (j = 0; j < c; ++j)
+	
+	for (i = 0; i < c; ++i)
+	for (j = 0; j < r; ++j)
+	{
+		cout << " " << trans[i][j];
+		if (j == r - 1)
+			
+	}
     return 0;
 }

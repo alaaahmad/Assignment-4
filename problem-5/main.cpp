0e517e75f60a6cc735cfd5eb581f50c128698340
alaaahmad <44245806+alaaahmad@users.noreply.github.com>
2018-12-15 20:54:34 +0200

    Update main.cpp

--- problem-5/main.cpp
+++ problem-5/main.cpp
@@ -2,5 +2,12 @@
 
 long factorial(int n) {
     // Write your code here
+    int i, n, factorial = 1;
+
+	cout << "Enter a positive integer: ";
+	cin >> n;
+
+	for (i = 1; i <= n; ++i) {
+		factorial *= i;  
     return 0;
 }

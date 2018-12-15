bc281362318f0fe6bbf41a1772f7efdb948c5272
alaaahmad <44245806+alaaahmad@users.noreply.github.com>
2018-12-15 20:12:56 +0200

    Update main.cpp

--- problem-2/main.cpp
+++ problem-2/main.cpp
@@ -8,5 +8,31 @@ int multiply(const int x[], const int y[], int n) {
 
 int *multiply(int x[], int *y[], int n) {
     // Write your code here
+    int a[1][10], b[10][10], mult[1][10], r1 = 1, n = 0, r2 = 0, c2 = 0, i, j, k;
+
+	r2 = n;
+	c2 = n;
+	for (i = 0; i < r1; ++i)
+	for (j = 0; j < n; ++j)
+	
+	for (i = 0; i < r2; ++i)
+	for (j = 0; j < c2; ++j)
+	
+	for (i = 0; i < r1; ++i)
+	for (j = 0; j < c2; ++j)
+	{
+		mult[i][j] = 0;
+	}
+	for (i = 0; i < r1; ++i)
+	for (j = 0; j < c2; ++j)
+	for (k = 0; k < n; ++k)
+	{
+		mult[i][j] += a[i][k] * b[k][j];
+	}
+	for (i = 0; i < r1; ++i)
+	for (j = 0; j < c2; ++j)
+	{
+		if (j == c2 - 1)
+	}
     return 0;
 }
